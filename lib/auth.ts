df26c7f065460bd5219d3b460e6549a67cdbaa49
rzalra <rizalnxa929@gmail.com>
2026-01-20 19:54:36 +0700

    fix: add baseURL to auth configuration

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,6 +4,8 @@ import { db } from "@/db"; // your drizzle instance
 import { account, session, user, verification } from "@/db/schema/auth";
 
 export const auth = betterAuth({
+    baseURL: process.env.BETTER_AUTH_URL,
+    
     database: drizzleAdapter(db, {
         provider: "pg", // or "mysql", "sqlite"
         schema: {
